716509bc30861fd6c18ffd58baa8ece438903219
elvisrodriguezc <elvisrodriguezc@gmail.com>
2022-04-19 05:13:11 -0500

    optimizacion

--- src/components/PaginationItem.jsx
+++ src/components/PaginationItem.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const PaginationItem = (props) => {
   return (
     <div>
-      <li className={props.curPage === props.page ? "page-item active" : "page-item"}><a className="page-link" onClick={props.handlePageChange}>{props.page}</a></li>
+      <li className={props.curPage === props.page ? "page-item active" : "page-item"}><button className="page-link" onClick={props.handlePageChange}>{props.page}</button></li>
     </div>
   )
 }
